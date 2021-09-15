ff73a1a6b093dc8cbcbb7529a1381a5a9540f4c4
unknown <99067718@mydavinci.nl>
2021-09-15 11:42:53 +0200

    fixed pizza calculator

--- PizzaCalculator.py
+++ PizzaCalculator.py
@@ -20,9 +20,28 @@ print("""
 |                             
 -------------------------------
 """)
-size = input("Would you like a Small, Medium or a Large Pizza? :")
-amo = int(input("How much " + size + " pizza's would you like to get? "))
+small = int(input("how much small pizza's would you like to have?: "))
+smalltotal = round(float(small) * float(SmallCost),2)
+print(str(small) + " pizza's with a cost of "+ str(smalltotal))
+
+medium = int(input("how much medium pizza's would you like to have?: "))
+mediumtotal = round(float(medium) * float(MediumCost),2)
+print(str(medium) + " pizza's with a cost of "+ str(mediumtotal))
+
+large = int(input("how much large pizza's would you like?: "))
+print(str(large) + " pizza's with a cost of "+ str(large * LargeCost))
+largetotal = round(float(large) * float(LargeCost),2)
+amo = small + medium + large
+totalcost = round((small * SmallCost) + (medium * MediumCost) + (largetotal),2)
 # Asks for completion of purchase
+print("------------------------------------------------")
+print("| kosten van bestelling")
+print("| small: "+ str(small) + " € " + str(SmallCost) + " = €" + str(smalltotal))
+print("| medium: "+ str(medium) + " € " + str(MediumCost) + " = €" + str(mediumtotal))
+print("| large: "+ str(large) + " € " +str(LargeCost) + " = €" + str(largetotal))
+print("|")
+print("Total:                €" + str(totalcost))
+print("------------------------------------------------")
 input("Thanks for your order, would you like to pay now, or will you purchase when delivery arrives? : ")
 input("The pizza(s) will be delivered at " + adress + " is that correct? ")
 print("Thanks, "+ name + " you will receive your pizza in " + str(amo * 4) + " minutes.")
